ac08ff11c4935591374e8cf1b77cee6d9c0f605c
zhangqi <zhangqi15@jd.com>
2018-10-12 18:38:36 +0800

    vuepress搭建技术文档

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ cd docs/.vuepress/dist
 git pull https://github.com/JDsecretFE/quist-ui.git master
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/JDsecretFE/quist-ui.git master
+git push -f https://github.com/JDsecretFE/quist-ui.git master:gh-pages
 
 cd -
