c5701139a237b14860da24a5b1ed8d37b5be4556
zhangqi <zhangqi15@jd.com>
2018-10-12 14:52:48 +0800

    vuepress搭建技术文档

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,6 @@ npm run docs:build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
-git init
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:jdsecretfe/quist-ui.git master
