6c8b25fba8583a910af3c7f3456d7c0aa6370903
许高博 <xugaobo@ZBMAC-C02VR0VYH.local>
2018-10-16 15:01:52 +0800

    完善webpack文件配置

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,8 +2,8 @@ var path = require('path')
 
 module.exports = {
   postHook: function(webpackConf, options){
-    webpackConf.resolve.alias = {
-      '@quist-ui': path.join(process.cwd(), 'node_modules/quist-ui/components')
-    }
+    webpackConf.resolve.alias = Object.assign(webpackConf.resolve.alias || {}, {
+      '@quist-ui': path.join(process.cwd(), 'components')
+    })
   }
 }
