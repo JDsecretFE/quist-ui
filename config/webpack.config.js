a386bf2789a51a4d1668a0c116f9b126f226940d
许高博 <xugaobo@ZBMAC-C02VR0VYH.local>
2018-10-16 14:55:56 +0800

    添加新组建

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,11 +2,8 @@ var path = require('path')
 
 module.exports = {
   postHook: function(webpackConf, options){
-    var rules = webpackConf.module.rules;
-    var extensions = webpackConf.resolve.extensions;
-    webpackConf.resolve.alias = {
-      '@quist-ui': path.join(process.cwd(), 'components'),
-      '@style': path.join(process.cwd(), 'style')
-    }
+    webpackConf.resolve.alias = Object.assign(webpackConf.resolve.alias || {}, {
+      '@quist-ui': path.join(process.cwd(), 'components')
+    })
   }
 }
