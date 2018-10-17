556a797ab139cfb5b5bd2eeeb9eba93cdb2d2182
许高博 <xugaobo@ZBMAC-C02VR0VYH.mshome.net>
2018-10-17 09:37:55 +0800

    拉取文档

--- config/webpack.config.js
+++
@@ -1,9 +0,0 @@
-var path = require('path')
-
-module.exports = {
-  postHook: function(webpackConf, options){
-    webpackConf.resolve.alias = Object.assign(webpackConf.resolve.alias || {}, {
-      '@quist-ui': path.join(process.cwd(), 'components')
-    })
-  }
-}
